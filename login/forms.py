5e3613e57429dfd6b0866d6fd98dd324feafe35c
Relhz <yevhenkudrya@gmail.com>
2016-03-28 17:46:23 +0300

    modified forms.py

--- login/forms.py
+++ login/forms.py
@@ -5,4 +5,4 @@ from django import forms
 class LoginForm(forms.Form):
 
 	username = forms.CharField(max_length=20)
-	password = forms.CharField(label='Password', max_length=20)
+	password = forms.CharField(label='Password', widget=forms.PasswordInput)
