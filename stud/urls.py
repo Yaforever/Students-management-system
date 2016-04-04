819710f00b40755b350f659fc135e49ba8e824e9
Relhz <yevhenkudrya@gmail.com>
2016-04-04 17:04:58 +0300

    update groupsAPI

--- stud/urls.py
+++ stud/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
 	url(r'^redactgroup/(?P<group_id>[0-9]+)/$', views.redactgroup),
 	url(r'^deletestudent/(?P<student_id>[0-9]+)/$', views.deletestudent),
 	url(r'^redactstudent/(?P<student_id>[0-9]+)/$', views.redactstudent),
-	url(r'^groupsAPI/$', views.group_list),
-	url(r'^groupsAPI/(?P<pk>[0-9]+)$', views.group_detail),
+	url(r'^groupsAPI/$', views.GroupList.as_view()),
+	url(r'^groupsAPI/(?P<pk>[0-9]+)$', views.GroupDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- stud/views.py
+++ stud/views.py
@@ -5,50 +5,17 @@ from django.contrib import auth
 from forms import GroupForm, StudentForm, DelGroup, DelStudent
 from login.forms import LoginForm
 from .serializers import GroupSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 
 
-
-@api_view(['GET', 'POST'])
-def group_list(request, format=None):
-
-    if request.method == 'GET':
-        groups = Group.objects.all()
-        serializer = GroupSerializer(groups, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = GroupSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class GroupList(generics.ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def group_detail(request, pk, format=None):
-
-	try:
-		groups = Group.objects.get(pk=pk)
-	except Group.DoesNotExist:
-		return Response(status=status.HTTP_404_NOT_FOUND)
-
-	if request.method == 'GET':
-		serializer = GroupSerializer(groups)
-		return Response(serializer.data)
-
-	elif request.method == 'PUT':
-		serializer = GroupSerializer(groups, data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-	elif request.method == 'DELETE':
-		groups.delete()
-		return Response(status=status.HTTP_204_NO_CONTENT)
+class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 
 def index(request):
