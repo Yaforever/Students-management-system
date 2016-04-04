459e63e9cfeb6dc7f20e5b4a196b8a520f573d06
Relhz <yevhenkudrya@gmail.com>
2016-04-04 14:16:30 +0300

    add groupsAPI

--- stud/urls.py
+++ stud/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
@@ -12,4 +13,8 @@ urlpatterns = [
 	url(r'^redactgroup/(?P<group_id>[0-9]+)/$', views.redactgroup),
 	url(r'^deletestudent/(?P<student_id>[0-9]+)/$', views.deletestudent),
 	url(r'^redactstudent/(?P<student_id>[0-9]+)/$', views.redactstudent),
+	url(r'^groupsAPI/$', views.group_list),
+	url(r'^groupsAPI/(?P<pk>[0-9]+)$', views.group_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
