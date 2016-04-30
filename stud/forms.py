54f04c449ffcc1388e92d34a7473802f87cbb208
Relhz <yevhenkudrya@gmail.com>
2016-04-30 13:15:06 +0300

    add Num error

--- stud/forms.py
+++ stud/forms.py
@@ -5,24 +5,12 @@ from django.forms import ModelForm
 from django.core.exceptions import ValidationError
 from models import Group, Student
 from django.forms.extras.widgets import SelectDateWidget
-import re
 
-
-def validate_groupname(self):
-	if not re.match(r'^([a-zA-Zа-яА-Я0-9]+)([ ]*|[-]?)([a-zA-Zа-яА-Я0-9]+)$', self.strip()):
-		raise ValidationError('Please, write only letters and digits, \
-			only one hyphenation. Something like "Group-32", "33 MM", "92f5gf9d4f" etc')
-
-
-def validate_name(self):
-	if not re.match(r'^(([a-zA-Zа-яА-Я]+)([ ]*|[-]?)([a-zA-Zа-яА-Я]+))*$', self.strip()):
-		raise ValidationError('Please, write only letters and single hyphenations. Something like \
-		"Name Surname", "Name Surname-Surname", "Name MiddleName Surname" etc')
 
 
 class GroupForm(ModelForm):
 	
-	groupname = forms.CharField(label='Group name', max_length=10, validators=[validate_groupname])
+	groupname = forms.CharField(label='Group name', max_length=10)
 
 	class Meta:
 		model = Group
@@ -31,9 +19,9 @@ class GroupForm(ModelForm):
 
 class StudentForm(ModelForm):
 	years = range(1970, 2030)
-	name = forms.CharField(label='Full name', max_length=30, validators=[validate_name])
+	name = forms.CharField(label='Full name', max_length=30)
 	num = forms.IntegerField(min_value=1, max_value=99999)
-	group = forms.ModelChoiceField(queryset=Group.objects.all())
+	group = forms.ModelChoiceField(queryset=Group.objects.all(), empty_label=None)
 	date = forms.DateField(widget=SelectDateWidget(years=years))
 
 	class Meta:
--- stud/static/stud/script.js
+++ stud/static/stud/script.js
@@ -40,7 +40,6 @@ $(document).ready(function(){
 		}
 	};
 
-
 	$('#id_date_year, #id_date_month').change(function(){
 		var y = $('#id_date_year').val()
 		var m = $('#id_date_month').val()
@@ -49,207 +48,7 @@ $(document).ready(function(){
 		date(y, m, d, dval)		
 	})
 
-	// group editing
-	function editgr(curr_id){
 
-		$('.delstud').show()
-		$('.redstud').show()	
-		$(".editst").hide();// hide student editting form
-		var redgroup
-		for (i = 0; i < $('.redgroup').length; i++){
-			if ($('.redgroup:eq(' + i + ')').val() == curr_id){
-				redgroup = $('.redgroup:eq(' + i + ')')
-			}
-		}
-		redgroup.next().after($('.editgr'));// insert group editing form
-		redgroup.siblings('.editgr').children('.mainstudent').html('');
-	
-		for (x = 0; x < $('.inp').length; x++){
-			if ($('.inp:eq(' + x + ')').html() == redgroup.val()){
-				redgroup.next().next().children('.mainstudent').append('<option>' + $('.inp:eq(' + x + ')').siblings('.sline').html().split('; ')[0] + '</option>');
-			}	
-		}
-		// set the initial values to the current values
-		redgroup.siblings('.editgr').children('.mainstudent').val(redgroup.prev().val())
-		redgroup.siblings('.editgr').children('.mainstudent').prev().val(redgroup.prev().prev().prev().html())
-		redgroup.siblings('.editgr').show()
-		$('.erroreditgr').html('')
-
-		$('.redactgroup').click(function(){
-			var its = $(this).parent()
-			var check = $(this).siblings('input').val()
-			// validation
-			if (check.match(/^([a-zA-Zа-яА-Я0-9]+)([ ]*|[-]?)([a-zA-Zа-яА-Я0-9]+)$/i) == null){
-				$('.erroreditgr').html('Please, write only letters and digits, \
-				only one hyphenation. Something like "Group-32", "33 MM", "92f5gf9d4f" etc')
-			}
-			else{
-				its.hide()	
-				$('.erroreditgr').html('')
-				
-				$.ajax({
-					url: '/groupsAPI/' + its.prev().prev().val() + '/',
-					type: 'PUT',
-					dataType: 'json',
-					data: {
-						'groupname': $(this).siblings('input').val(),
-						'mainstudent': $(this).siblings('select').val(),
-					},
-					success: function(json){
-						for (i = 0; i < $('.line').length; i++){
-							if (its.prev().prev().val() == json.id){
-								its.prev().prev().prev().prev().prev().html(json.groupname)
-							}
-						}
-						
-						for (i = 0; i < $('.editst select:eq(0) option').length; i++){
-							if ($('.editst select:eq(0) option:eq(' + i + ')').val() == redgroup.val()){
-								$('.editst select:eq(0) option:eq(' + i + ')').html(json.groupname)
-								$('.add_st select:eq(0) option:eq(' + i + ')').html(json.groupname)
-							}
-						}
-					},
-				})
-			}
-		})
-	}
-	// group deleting
-	function delgr(curr_id){
-		
-		$.ajax({
-			url: '/groupsAPI/' + curr_id + '/',
-			type: 'DELETE',
-			success: function(data){
-				
-				$('.editst').hide()
-				
-				var it;
-				for (i = 0; i < $('.delgroup').length; i++){
-					if($('.delgroup:eq(' + i + ')').val() == curr_id){
-						it = $('.delgroup:eq(' + i + ')')
-					}
-				};
-				function ds(){
-					for (i = 0; i < $('.inp').length; i++){
-						if ($('.inp:eq(' + i + ')').html() == it.val()){
-							$('.inp:eq(' + i + ')').siblings('.editst').appendTo($('.main'))
-							$('.inp:eq(' + i + ')').parent().remove();
-							return ds()
-						}
-					};
-				};
-				ds()
-
-				it.siblings('.editgr').hide();
-				it.prev().remove();
-				it.prev().remove();
-				it.next().remove();
-				it.next().remove();
-				it.next().remove();
-				it.remove();
-				
-				$('.add_st select:eq(0) option:last-child').remove();
-				$('.editst select:eq(0) option:last-child').remove();
-			}
-		});
-	};
-	// student editing
-	function redst(value){
-		$('.err_edit_st').html('')
-		var it 
-		for (i = 0; i < $('.redstud').length; i++){
-			if($('.redstud:eq(' + i + ')').next().next().next().html() == value){
-				it = $('.redstud:eq(' + i + ')')
-			}
-		}
-		$('.editst').hide()
-		$(".editgr").hide()// hide group editing form
-		$('.delstud').show()
-		$('.redstud').show()
-
-		it.prev().hide()
-		it.hide()
-		it.next().after($('.editst')[0]);// insert student edtiting form
-		it.siblings(".editst:eq(0)").show();
-		var currgroup = it.next().next().next().html()
-		var thisinf = it.prev().prev().html().split('; ')
-		var day = thisinf[2].split('-')[2]
-		if(day[0] == 0){
-			day = day[1]
-		}
-		//set initial values to current values
-		it.siblings(".editst").children('#id_name').val(thisinf[0])
-		it.siblings(".editst").children('#id_num').val(thisinf[1])
-		it.siblings(".editst").children('#id_group').val(currgroup)
-		it.siblings(".editst").children('#id_date_month').val(thisinf[2].split('-')[1])
-		it.siblings(".editst").children('#id_date_day').val(day)
-		it.siblings(".editst").children('#id_date_year').val(thisinf[2].split('-')[0])
-
-		var y = it.siblings(".editst").children('#id_date_year').val()
-		var m = it.siblings(".editst").children('#id_date_month').val()
-		var d = it.siblings(".editst").children('#id_date_day').children()
-		var dval = it.siblings(".editst").children('#id_date_day')
-
-		date(y, m, d, dval)
-
-		$('#id_date_year, #id_date_month').change(function(){
-			var y = $(this).siblings('#id_date_year').val();
-			var m = $(this).siblings('#id_date_month').val();
-			if (y == undefined){
-				y = $(this).val()
-			}
-			if (m == undefined){
-				m = $(this).val()
-			}
-			var d = $(this).siblings('#id_date_day').children();
-			var dval = $(this).siblings('#id_date_day');
-			date(y, m, d, dval)
-		})
-	
-		$('.redactstudent').click(function(){
-			$('.delstud').show()
-			$('.redstud').show()
-			var it = $(this).parent()
-			var month = $(this).siblings('#id_date_month').val()
-			var day = $(this).siblings('#id_date_day').val()
-			var year = $(this).siblings('#id_date_year').val()
-			var checkname = it.children('#id_name').val()
-			// validation
-			if (checkname.match(/^(([a-zA-Zа-яА-Я]+)([ ]*|[-]?)([a-zA-Zа-яА-Я]+))*$/i) == null){
-				$('.err_edit_st').html('Please, write only letters and single hyphenations. Something like \
-			"Name Surname", "Name Surname-Surname", "Name MiddleName Surname" etc')
-			}
-			else{
-				$('.err_edit_st').html('')
-				$.ajax({
-					url: '/studentsAPI/' + it.next().next().html() + '/',
-					type: 'PUT',
-					dataType: 'json',
-					data:{
-						'name': $(this).siblings('#id_name').val(),
-						'num': $(this).siblings('#id_num').val(),
-						'group': $(this).siblings('#id_group').val(),
-						'date': year + '-' + month + '-' + day,
-					},
-					
-					success: function(json){
-						if (it.next().html() != json.group){
-							for (i = 0; i < $('.delgroup').length; i++){
-								// if the student is edited and his group has been changed, then insert the student to the his new group
-								if ($('.delgroup:eq(' + i + ')').val() == json.group){
-									$('.redgroup:eq(' + i + ')').next().after(it.parent())
-								}
-							}
-						}
-						
-						it.prev().prev().prev().prev().html(json.name + '; ' + json.num + '; ' + json.date)
-						it.next().html(json.group)
-						it.hide()
-					}
-				})
-			}
-		})
-	}
 
 	function getCookie(name) {
 		var cookieValue = null;
@@ -281,7 +80,7 @@ $(document).ready(function(){
 	});
 	//  group adding
 	$('.addinggroup').click(function(){
-		var check = $(this).siblings('#id_groupname').val()
+		var check = $(this).siblings('#id_groupname').val().trim()
 		//validation
 		if (check.match(/^([a-zA-Zа-яА-Я0-9]+)([ ]*|[-]?)([a-zA-Zа-яА-Я0-9]+)$/i) == null){
 			$('.erroraddgr').html('Please, write only letters and digits, \
@@ -297,7 +96,7 @@ $(document).ready(function(){
 					'groupname': $(this).siblings('#id_groupname').val(),
 				},
 				success: function(data){
-					
+					// if succes, display added group
 					$('.add_st select:eq(0)').append('<option class="added" value="' + data.id + '" >' + data.groupname + '</option>')
 					$('.editst select:eq(0)').append('<option class="added" value="' + data.id + '" >' + data.groupname + '</option>')
 					$('.ul_gr').append('<br><li class="line"></li>')
@@ -311,19 +110,64 @@ $(document).ready(function(){
 					$('.delgroup:eq(' + eq + ')').val(data.id)
 					$('.redgroup:eq(' + eq + ')').val(data.id)
 					$('.main:eq(' + eq + ')').val(data.mainstudent)
-					// group deleting
-
 				}
 			})
 		}
 	})
-
+	// group deleting
 	$( ".delgroup" ).live( "click", function() {
-		delgr($(this).val())
-	});	
+		var it = $(this)
+		$.ajax({
+			url: '/groupsAPI/' + $(this).val() + '/',
+			type: 'DELETE',
+			success: function(data){
+				
+				$('.editst').hide()
+				// delete students of this group
+				function ds(){
+					for (i = 0; i < $('.inp').length; i++){
+						if ($('.inp:eq(' + i + ')').html() == it.val()){
+							$('.inp:eq(' + i + ')').siblings('.editst').appendTo($('.main'))
+							$('.inp:eq(' + i + ')').parent().remove();
+							return ds()
+						}
+					};
+				};
+				ds()
 
+				it.siblings('.editgr').hide();
+				it.prev().remove();
+				it.prev().remove();
+				it.next().remove();
+				it.next().remove();
+				it.next().remove();
+				it.remove();
+				
+				$('.add_st select:eq(0) option:last-child').remove();
+				$('.editst select:eq(0) option:last-child').remove();
+			}
+		});
+	});	
+	// group editing
 	$( ".redgroup" ).live( "click", function() {
-		editgr($(this).val())
+	
+		$('.delstud').show()
+		$('.redstud').show()	
+		$(".editst").hide();// hide student editting form
+
+		$(this).next().after($('.editgr'));// insert group editing form
+		$(this).siblings('.editgr').children('.mainstudent').html('');
+	
+		for (x = 0; x < $('.inp').length; x++){
+			if ($('.inp:eq(' + x + ')').html() == $(this).val()){
+				$(this).next().next().children('.mainstudent').append('<option>' + $('.inp:eq(' + x + ')').siblings('.sline').html().split('; ')[0] + '</option>');
+			}	
+		}
+		// set the initial values to the current values
+		$(this).siblings('.editgr').children('.mainstudent').val($(this).prev().val())
+		$(this).siblings('.editgr').children('.mainstudent').prev().val($(this).prev().prev().prev().html())
+		$(this).siblings('.editgr').show()
+		$('.erroreditgr').html('')
 	});	
 
 	$('.delstud').live("click", function(){
@@ -332,16 +176,143 @@ $(document).ready(function(){
 			url: '/studentsAPI/' + $(this).siblings('.idstud').html()  + '/',
 			type: 'DELETE',
 			success: function(){
-				it.parent().remove()
+				it.parent().hide()
+				it.parent().appendTo($('.main'))
 			}
 		})
 	})	
-
+	// student editing
 	$(".redstud").live('click', function(){
-		$(".editgr").hide()
+
+		$('.editst').hide()
+		$(".editgr").hide()// hide group editing form
+		$('.delstud').show()
+		$('.redstud').show()
+
 		$(this).prev().hide()
-		redst($(this).next().next().next().html())
+		$(this).hide()
+		$(this).next().after($('.editst')[0]);// insert student edtiting form
+		$(this).siblings(".editst:eq(0)").show();
+		var currgroup = $(this).next().next().next().html()
+		var thisinf = $(this).prev().prev().html().split('; ')
+		var day = thisinf[2].split('-')[2]
+		if(day[0] == 0){
+			day = day[1]
+		}
+		//set initial values to current values
+		$(this).siblings(".editst").children('#id_name').val(thisinf[0])
+		$(this).siblings(".editst").children('#id_num').val(thisinf[1])
+		$(this).siblings(".editst").children('#id_group').val(currgroup)
+		$(this).siblings(".editst").children('#id_date_month').val(thisinf[2].split('-')[1])
+		$(this).siblings(".editst").children('#id_date_day').val(day)
+		$(this).siblings(".editst").children('#id_date_year').val(thisinf[2].split('-')[0])
+
+		var y = $(this).siblings(".editst").children('#id_date_year').val()
+		var m = $(this).siblings(".editst").children('#id_date_month').val()
+		var d = $(this).siblings(".editst").children('#id_date_day').children()
+		var dval = $(this).siblings(".editst").children('#id_date_day')
+
+		date(y, m, d, dval)
+
+		$('#id_date_year, #id_date_month').change(function(){
+			var y = $(this).siblings('#id_date_year').val();
+			var m = $(this).siblings('#id_date_month').val();
+			if (y == undefined){
+				y = $(this).val()
+			}
+			if (m == undefined){
+				m = $(this).val()
+			}
+			var d = $(this).siblings('#id_date_day').children();
+			var dval = $(this).siblings('#id_date_day');
+			date(y, m, d, dval)
+		})
 	})
+
+	$('.redactstudent').live('click', function(){
+		$('.delstud').show()
+		$('.redstud').show()
+		var it = $(this).parent()
+		var month = $(this).siblings('#id_date_month').val()
+		var day = $(this).siblings('#id_date_day').val()
+		var year = $(this).siblings('#id_date_year').val()
+		var checkname = it.children('#id_name').val().trim()
+		var num = it.children('#id_num').val()
+		// validation
+		if (checkname.match(/^(([a-zA-Zа-яА-Я]+)([ ]*|[-]?)([a-zA-Zа-яА-Я]+))*$/i) == null){
+			$('.err_edit_st').html('Name require only letters and single hyphenations. Something like \
+		"Name Surname", "Name Surname-Surname", "Name MiddleName Surname" etc')
+		}
+		else if (num == '' & checkname.length > 0){
+			$('.err_edit_st').html('Wrong num')
+		}
+		else{
+			$('.err_edit_st').html('');
+			$.ajax({
+				url: '/studentsAPI/' + it.next().next().html() + '/',
+				type: 'PUT',
+				dataType: 'json',
+				data:{
+					'name': $(this).siblings('#id_name').val(),
+					'num': $(this).siblings('#id_num').val(),
+					'group': $(this).siblings('#id_group').val(),
+					'date': year + '-' + month + '-' + day,
+				},
+				
+				success: function(json){
+					if (it.next().html() != json.group){
+						for (i = 0; i < $('.delgroup').length; i++){
+							// if the student is edited and his group has been changed, then insert the student to the his new group
+							if ($('.delgroup:eq(' + i + ')').val() == json.group){
+								$('.redgroup:eq(' + i + ')').next().after(it.parent())
+							}
+						}
+					}
+					
+					it.prev().prev().prev().prev().html(json.name + '; ' + json.num + '; ' + json.date)
+					it.next().html(json.group)
+					it.hide()
+				}
+			})
+		}
+	})
+
+	$('.redactgroup').live('click', function(){
+
+		var it = $(this).parent().prev().prev().val()
+		var its = $(this).parent()
+		var check = $(this).siblings('input').val().trim()
+		// validation
+		if (check.match(/^([a-zA-Zа-яА-Я0-9]+)([ ]*|[-]?)([a-zA-Zа-яА-Я0-9]+)$/i) == null){
+			$('.erroreditgr').html('Please, write only letters and digits, \
+			only one hyphenation. Something like "Group-32", "33 MM", "92f5gf9d4f" etc')
+		}
+		else{
+			its.hide()	
+			$('.erroreditgr').html('')
+			
+			$.ajax({
+				url: '/groupsAPI/' + its.prev().prev().val() + '/',
+				type: 'PUT',
+				dataType: 'json',
+				data: {
+					'groupname': check,
+					'mainstudent': $(this).siblings('select').val(),
+				},
+				success: function(json){
+	
+					its.prev().prev().prev().prev().prev().html(json.groupname)
+					for (i = 0; i < $('.editst select:eq(0) option').length; i++){
+						if ($('.editst select:eq(0) option:eq(' + i + ')').val() == it){
+							$('.editst select:eq(0) option:eq(' + i + ')').html(json.groupname)
+							$('.add_st select:eq(0) option:eq(' + i + ')').html(json.groupname)
+						}
+					}
+				},
+			})
+		}
+	})
+
 	$.ajax({
 		url: '/groupsAPI/',
 		type: 'GET',
@@ -383,34 +354,44 @@ $(document).ready(function(){
 					$('.sline').after('<button class="delstud" \
 					 alt="Delete"></button><button \
 					class="redstud" alt="Redact"></button><br class="brr"></div>')
-
 				},			
 			})
 		}
 	})
-
+	// max length of the value of a "num" field equal 5
+	$('#id_num').live('input', function (){
+		if ($(this).val().length > 5){
+			$(this).val($(this).val().substr(0,5))
+		}
+	});
 
 	// student adding
 	$('.addingstudent').click(function (){
 		var month = $(this).siblings('#id_date_month').val()
 		var day = $(this).siblings('#id_date_day').val()
 		var year = $(this).siblings('#id_date_year').val()
-		var checkname = $(this).siblings('#id_name').val()
+		var checkname = $(this).siblings('#id_name').val().trim()
+		var num = $(this).siblings('#id_num').val()
 		var gr = $(this).siblings('#id_group').val()
 		// validation
 		if (checkname.match(/^(([a-zA-Zа-яА-Я]+)([ ]*|[-]?)([a-zA-Zа-яА-Я]+))*$/i) == null){
-			$('.erroraddst').html('Please, write only letters and single hyphenations. Something like \
+			$('.erroraddst').html('Name require only letters and single hyphenations. Something like \
 		"Name Surname", "Name Surname-Surname", "Name MiddleName Surname" etc')
 		}
 		else{
-			$('.erroraddst').html('');
+			if (num == '' & checkname.length > 0){
+				$('.erroraddst').html('Wrong num')
+			}
+			else {
+				$('.erroraddst').html('');
+			}
 			$.ajax({
 				url: '/studentsAPI/',
 				type: 'POST',
 				dataType: 'json',
 				data: {
-					'name': $(this).siblings('#id_name').val(),
-					'num': $(this).siblings('#id_num').val(),
+					'name': checkname,
+					'num': num,
 					'group': $(this).siblings('#id_group').val(),
 					'date': year + '-' + month + '-' + day,
 				},
@@ -428,7 +409,6 @@ $(document).ready(function(){
 						<button class="delstud" alt="Delete"></button><button \
 					class="redstud" alt="Redact"></button><br class="brr"><button class="inp" style="display: none">' + gr + '</button> \
 						<button class="idstud" style="display: none">' + data.id + '</button></div>');
-
 				},			
 			})
 		}	
